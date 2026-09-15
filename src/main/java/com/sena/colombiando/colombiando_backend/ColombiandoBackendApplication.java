d8a636d08d9735412a3725eef98907db46841259
alexvirimi <alexvirimi@gmail.com>
2026-09-14 23:28:24 -0500

    Initial commit

---
+++ src/main/java/com/sena/colombiando/colombiando_backend/ColombiandoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sena.colombiando.colombiando_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColombiandoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColombiandoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/colombiando/colombiando_backend/ColombiandoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.colombiando.colombiando_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColombiandoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
